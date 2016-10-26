48d64e715b1d8194d8bd738ab11a9afb07b6bab8
Mohammed Irfan <irfan@hamon.in>
2016-10-26 10:49:50 +0530

    Fix compile.sh in python

--- python/compile.sh
+++ python/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pip install cython
-CFLAGS="-I../src" LDFLAGS="-L../src ../usrsctp/usrsctplib/.libs" python setup.py build_ext -i
+CFLAGS="-I../src" LDFLAGS="-L../src/usrsctp/usrsctplib/.libs" python setup.py build_ext -i
