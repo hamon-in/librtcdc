88704897179a370d8624a474186e5daec2c92da9
Mohammed Irfan <irfan@hamon.in>
2017-01-31 16:09:30 +0530

    Added library dirs and header files.

--- python/api_build.py
+++ python/api_build.py
@@ -9,72 +9,13 @@ dc_open = False
 
 ffibuilder = FFI()
 
-ffibuilder.cdef("""
-typedef void (*rtcdc_on_candidate_cb)(struct rtcdc_peer_connection *, const char *, void *);
-typedef void (*rtcdc_on_connect_cb)(struct rtcdc_peer_connection *, void *);
-typedef void (*rtcdc_on_message_cb)(struct rtcdc_data_channel *, int, void *, size_t, void *);
-typedef void (*rtcdc_on_close_cb)(struct rtcdc_data_channel *, void *);
-
-typedef void (*rtcdc_on_channel_cb)(struct rtcdc_peer_connection *, struct rtcdc_data_channel *, void *);
-typedef void (*rtcdc_on_open_cb)(struct rtcdc_data_channel *, void *);
-
-typedef struct rtcdc_data_channel {
-      uint8_t type;
-      uint16_t priority;
-      uint32_t rtx;
-      uint32_t lifetime;
-      char *label;
-      char *protocol;
-      int state;
-      uint16_t sid;
-      struct sctp_transport *sctp;
-      rtcdc_on_open_cb on_open;
-      rtcdc_on_message_cb on_message;
-      rtcdc_on_close_cb on_close;
-      void *user_data;
-    } rtcdc_data_channel;
-    
-typedef struct rtcdc_peer_connection {
-char *stun_server;
-      uint16_t stun_port;
-      int exit_thread;
-      struct rtcdc_transport *transport;
-      int initialized;
-      int role;
-      struct rtcdc_data_channel *channels[128];
-      rtcdc_on_channel_cb on_channel;
-      rtcdc_on_candidate_cb on_candidate;
-      rtcdc_on_connect_cb on_connect;
-      void *user_data;
-    } rtcdc_peer_connection;
-
-
-
-
-
-
-struct rtcdc_data_channel *
-rtcdc_create_data_channel(struct rtcdc_peer_connection *peer,
-                          const char *label, const char *protocol,
-                          rtcdc_on_open_cb on_open,
-                          rtcdc_on_message_cb on_message,
-                          rtcdc_on_close_cb on_close,
-                          void *user_data);
-
-struct rtcdc_peer_connection *
-rtcdc_create_peer_connection(rtcdc_on_channel_cb,
-                             rtcdc_on_candidate_cb,
-                             rtcdc_on_connect_cb,
-                             const char *, uint16_t,
-                             void *);
-
-
-    """)
 
 ffibuilder.set_source("_apilib",
                       r"""
 #include <sys/types.h>
 #include <rtcdc.h>
+#include <dcep.h>
+#include <sctp.h>
 #ifdef _WIN32
 #define _CRT_SECURE_NO_WARNINGS
 #include <WinSock2.h>
@@ -230,7 +171,8 @@ rtcdc_create_peer_connection(rtcdc_on_channel_cb on_channel,
 
   return peer;
 }""",
-include_dirs = ["../src/"])
+include_dirs = ["../src/", "../src/usrsctp/usrsctplib/",
+        "/usr/include/glib-2.0", "/usr/lib64/glib-2.0/include"])
 
 RTCDC_CHANNEL_STATE_CLOSED = 0
 RTCDC_CHANNEL_STATE_CONNECTING = 1
