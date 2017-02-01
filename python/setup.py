001b9dc1cb5d75ce05a214f62f558f76ab1a37fb
Labeeb Ibrahim <labeeb@hamon.in>
2017-02-01 13:15:07 +0530

    Adds setup.py for building the library

--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,16 @@
 # Copyright (c) 2015 Xiaohan Song <chef@dark.kitchen>
 # This file is licensed under a BSD license.
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
-setup(
-  ext_modules = cythonize([Extension("pyrtcdc", 
-                                     ["pyrtcdc.pyx"], 
-                                     include_dirs=['../src/'],
-                                     libraries=["rtcdc", "nice"],
-                                     library_dirs=["../src/vendor/build"]
-                                     ),
-                          ], gdb_debug=True)
-)
+setup(name = "pyrtcdc",
+      version = "0.1",
+      packages = find_packages(),
+      setup_requires=["cffi>=1.0.0"],
+      cffi_modules=["pyrtcdc_build.py:ffibuilder"],
+      install_requires = ["cffi>=1.0.0"],
+      author = "Labeeb Ibrahim, Mohammed Irfan",
+      author_email = "labeeb@hamon.in, irfan@hamon.in",
+      description = "Python bindings for the librtcdc library using Out of line API mode of CFFI",
+      url = "http://github.com/hamon-n/librtcdc",
+      )
