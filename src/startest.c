7d7807cd02f000bcc87a687b8a8c3d93242ae7c8
Mohammed Irfan <irfan@hamon.in>
2017-03-13 18:28:41 +0530

    Include two callbacks within four peer connections.
    Fourth peer connection is to be considered as same as first
    peer connection for the star topology.

--- src/startest.c
+++ src/startest.c
@@ -13,32 +13,40 @@ void rtcdc_e_loop(void *args) {
 }
 
 int main() {
-    struct rtcdc_peer_connection *pc1, *pc2, *pc3;
-    void onconnect(struct rtcdc_peer_connection *peer, void *user_data) {
-        printf("\nOnConnect fired");
+    struct rtcdc_peer_connection *pc1, *pc2, *pc3, *pc4;
+    void onconnectA(struct rtcdc_peer_connection *peer, void *user_data) {
+        printf("\nOnConnect of 1->2's");
     }
-    
+    void onconnectB(struct rtcdc_peer_connection *peer, void *user_data) {
+        printf("\nOnConnect of '1'->3's");
+    }
     void *user_data=NULL;
-    printf("\nCreating peer connection...\n");
 
-    pc1 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
+    pc1 = rtcdc_create_peer_connection(NULL, NULL, onconnectA,
             "stun.services.mozilla.com", 3478, user_data);
     
-    pc2 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
+    pc2 = rtcdc_create_peer_connection(NULL, NULL, onconnectA,
             "stun.services.mozilla.com", 3478, user_data);
 
-    pc3 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
+    pc3 = rtcdc_create_peer_connection(NULL, NULL, onconnectB,
+            "stun.services.mozilla.com", 3478, user_data);
+    
+    pc4 = rtcdc_create_peer_connection(NULL, NULL, onconnectB,
             "stun.services.mozilla.com", 3478, user_data);
 
-    handshake(pc1, pc2); //onConnect would fire after sometime for peer conn 1->2
-    handshake(pc1, pc3); //onConnect doesn't fire for this peer conn 1->3
-
-    pthread_t tid1, tid2, tid3; void *res1;
+    pthread_t tid1, tid2, tid3, tid4; void *res1, *res2, *res3, *res4;
     pthread_create(&tid1, NULL, (void *)rtcdc_e_loop, (void *) pc1);
     pthread_create(&tid2, NULL, (void *)rtcdc_e_loop, (void *) pc2);
     pthread_create(&tid3, NULL, (void *)rtcdc_e_loop, (void *) pc3);
+    pthread_create(&tid4, NULL, (void *)rtcdc_e_loop, (void *) pc4);
+
+    handshake(pc1, pc2); // 1->2
+    handshake(pc4, pc3); //A new connection considered as 1->3
+    // Programmer should keep track of 'peer connections' and consider pc1 and pc4 as 'same' peer '1'.
+
     pthread_join(tid1, &res1);
-    pthread_join(tid2, &res1);
-    pthread_join(tid3, &res1);
+    pthread_join(tid2, &res2);
+    pthread_join(tid3, &res3);
+    pthread_join(tid4, &res4);
     return 0;
 }
