3b392d4e570c9af6275c3b9b2bcb305ee5882a17
Mohammed Irfan <irfan@hamon.in>
2017-03-13 16:08:04 +0530

    Makes startest program simpler.

--- src/startest.c
+++ src/startest.c
@@ -14,46 +14,24 @@ void rtcdc_e_loop(void *args) {
 
 int main() {
     struct rtcdc_peer_connection *pc1, *pc2, *pc3;
-    void onmessage(struct rtcdc_data_channel *channel, int datatype, void *data, size_t len, void *user_data) {
-        printf("\nData received: %s\n", (char *)data);
-    }
-    void onopen(struct rtcdc_data_channel *channel, void *user_data) {
-        printf("\nDataChannel opened.\n");
-        //char *message = "Hello";
-        //rtcdc_send_message(channel, RTCDC_DATATYPE_STRING, message, strlen(message));
-    }
-    void onclose(struct rtcdc_data_channel *channel, void *user_data) {
-        printf("\nDataChannel closed\n");
-    }
     void onconnect(struct rtcdc_peer_connection *peer, void *user_data) {
         printf("\nOnConnect fired");
-        if (peer->role == RTCDC_PEER_ROLE_CLIENT) {
-            char label[10];
-            snprintf(label, 10, "test-dc-%d", peer->role);
-            //rtcdc_create_data_channel(peer, label, "", onopen, onmessage, onclose, (void *)peer);
-        }
     }
-    void onchannel(struct rtcdc_peer_connection *peer, struct rtcdc_data_channel *channel, void *user_data) {
-        printf("\nChannel created: %s\n", channel->label); 
-        channel->on_message = onmessage;
-    }
-    void oncandidate(struct rtcdc_peer_connection *peer, const char *candidate, void *user_data) {
-        //printf("\nCandidate found:\n%s\n", g_base64_encode(candidate, strlen(candidate)));
-    }
+    
     void *user_data=NULL;
     printf("\nCreating peer connection...\n");
 
-    pc1 = rtcdc_create_peer_connection(onchannel, oncandidate, onconnect,
+    pc1 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
             "stun.services.mozilla.com", 3478, user_data);
     
-    pc2 = rtcdc_create_peer_connection(onchannel, oncandidate, onconnect,
+    pc2 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
             "stun.services.mozilla.com", 3478, user_data);
 
-    pc3 = rtcdc_create_peer_connection(onchannel, oncandidate, onconnect,
+    pc3 = rtcdc_create_peer_connection(NULL, NULL, onconnect,
             "stun.services.mozilla.com", 3478, user_data);
 
-    handshake(pc1, pc2);
-    handshake(pc1, pc3);
+    handshake(pc1, pc2); //onConnect would fire after sometime for peer conn 1->2
+    handshake(pc1, pc3); //onConnect doesn't fire for this peer conn 1->3
 
     pthread_t tid1, tid2, tid3; void *res1;
     pthread_create(&tid1, NULL, (void *)rtcdc_e_loop, (void *) pc1);
