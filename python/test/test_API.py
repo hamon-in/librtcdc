140b99d085d03e654e709edcaf9927a8c77c387b
Mohammed Irfan <irfan@hamon.in>
2017-03-15 11:23:29 +0530

    Add exception to test_API

--- python/test/test_API.py
+++ python/test/test_API.py
@@ -3,17 +3,22 @@ from unittest.mock import Mock
 from time import sleep
 
 def test_star():
-    from ..rtcdc import Peer
+    from ..rtcdc import Peer, connection_error
     on_message = Mock()
 
-    peerA = Peer(on_message = None)
-    peerB = Peer(on_message = on_message)
+    peerA = Peer()
+    peerB = Peer()
     peerC = Peer(on_message = on_message)
 
     peerA.connect(peerB)
     peerA.connect(peerC)
 
-    peerA.send_message(peerC, "message") #Exception if A is not connected to C
-    print("Waiting 5 seconds just to be safe")
-    sleep(5)
-    on_message.assert_called_with(msg == "message")
+    try:
+        peerA.send_message(peerC, "message")
+        print("Waiting 5 seconds just to be safe")
+        sleep(5)
+        on_message.assert_called_with(msg == "message")
+    except connection_error as e:
+        print(e.message)
+
+    
